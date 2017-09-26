f8360cf8fea83ec2113a4679774dfd9eca2e18bb
GRIM <fineex@outlook.com>
2017-09-26 10:10:28 +0800

    s

---
+++ Ofo_Swift/Ofo_Swift/MenuController.swift
@@ -0,0 +1,34 @@
+//
+//  MenuController.swift
+//  Ofo_Swift
+//
+//  Created by Dwt on 2017/9/26.
+//  Copyright © 2017年 Dwt. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UITableViewController {
+
+    @IBOutlet weak var avator: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var identify: UILabel!
+    @IBOutlet weak var credit: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+}
