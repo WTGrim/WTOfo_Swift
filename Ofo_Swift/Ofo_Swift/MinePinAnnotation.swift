63fdad93806d156a2919ca6c7d88312bda471fb2
GRIM <fineex@outlook.com>
2017-09-27 17:52:08 +0800

    红包车和普通车检索

---
+++ Ofo_Swift/Ofo_Swift/MinePinAnnotation.swift
@@ -0,0 +1,14 @@
+//
+//  MinePinAnnotation.swift
+//  Ofo_Swift
+//
+//  Created by Dwt on 2017/9/27.
+//  Copyright © 2017年 Dwt. All rights reserved.
+//
+
+import Foundation
+
+class MinePinAnnotation: MAPointAnnotation {
+    
+    
+}
--- Ofo_Swift/Ofo_Swift/ViewController.swift
+++ Ofo_Swift/Ofo_Swift/ViewController.swift
@@ -9,16 +9,49 @@
 import UIKit
 import SWRevealViewController
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , MAMapViewDelegate, AMapSearchDelegate{
 
+    //底部控制台
     @IBOutlet weak var panelView: UIView!
+    var mapView:MAMapView!
+    var search:AMapSearchAPI!
+    var minePin:MinePinAnnotation!
+    var minePinView:MAPinAnnotationView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    
+    //定位
+    @IBAction func locationClick(_ sender: UIButton) {
         
+        searchNearBike()
+    }
+    
+}
+
+//Mark:-自定义的相关方法
+extension ViewController{
+    
+    fileprivate func setupUI() {
         navigationItem.titleView = UIImageView(image:UIImage.init(named: "Login_Logo_117x25_"))
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         navigationController?.navigationBar.tintColor = UIColor.black
+        
+        //高德地图
+        mapView = MAMapView(frame:view.bounds)
+        mapView.delegate = self
+        mapView.zoomLevel = 17
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        view.addSubview(mapView)
+        
+        search = AMapSearchAPI()
+        search.delegate = self
+        
+        view.bringSubview(toFront: panelView)
         
         if let revealVC = revealViewController(){
             revealVC.rearViewRevealWidth = 280
@@ -27,11 +60,100 @@ class ViewController: UIViewController {
             view.addGestureRecognizer(revealVC.panGestureRecognizer())
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    //用餐馆模拟成小黄车
+    fileprivate func searchCustomLocation(_ center:CLLocationCoordinate2D) {
+        
+        let request = AMapPOIAroundSearchRequest()
+        request.location = AMapGeoPoint.location(withLatitude: CGFloat(center.latitude), longitude: CGFloat(center.longitude))
+        request.keywords = "餐馆"
+        request.radius = 600
+        request.requireExtension = true
+        search.aMapPOIAroundSearch(request)
+    }
+    
+    //搜索周边小黄车
+    fileprivate func searchNearBike() {
+        searchCustomLocation(mapView.userLocation.coordinate)
     }
 
+}
 
+//Mark:-相关代理
+extension ViewController{
+    
+    
+    //poi检索完成后的回调
+    func onPOISearchDone(_ request: AMapPOISearchBaseRequest!, response: AMapPOISearchResponse!) {
+        
+        guard response.count > 0 else {
+            print("没有小黄车")
+            return
+        }
+        
+        var annotations : [MAPointAnnotation] = []
+        annotations = response.pois.map{
+            let annotation = MAPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees($0.location.latitude), longitude: CLLocationDegrees($0.location.longitude))
+            if $0.distance < 50 {//红包车
+                annotation.title = "红包区域开锁小黄车"
+                annotation.subtitle = "骑行10分钟可获得现金红包"
+            }else{
+                annotation.title = "正常可用"
+            }
+            return annotation
+        }
+        
+        mapView.addAnnotations(annotations)
+        mapView.showAnnotations(annotations, animated: true)
+        
+    }
+    
+    //自定义大头针
+    func mapView(_ mapView: MAMapView!, viewFor annotation: MAAnnotation!) -> MAAnnotationView! {
+        
+        //用户的位置，不需要自定义
+        if annotation is MAUserLocation{
+            return nil
+        }
+        
+        if annotation is MinePinAnnotation{//如果是我的位置
+            let pinId = "minePin"
+            var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: pinId) as? MAPinAnnotationView
+            if pinView == nil{
+                pinView = MAPinAnnotationView(annotation: annotation, reuseIdentifier: pinId)
+            }
+            pinView?.image = UIImage(named:"homePage_wholeAnchor_24x37_")
+            pinView?.canShowCallout = false
+            minePinView = pinView
+            return pinView
+        }
+        
+        let reuseId = "annotationId"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MAPinAnnotationView
+        if annotationView == nil{
+            annotationView = MAPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+        }
+        
+        if annotation.title == "正常可用" {
+            annotationView?.image = UIImage(named: "HomePage_nearbyBike_33x36_")
+        }else{
+            annotationView?.image = UIImage(named:"HomePage_nearbyBikeRedPacket_33x36_")
+        }
+        annotationView?.canShowCallout = true
+        annotationView?.animatesDrop = true
+        return annotationView
+    }
+    
+    func mapInitComplete(_ mapView: MAMapView!) {
+        
+        minePin = MinePinAnnotation()
+        minePin.coordinate = mapView.centerCoordinate
+        minePin.lockedScreenPoint = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
+        minePin.isLockedToScreen = true
+        mapView.addAnnotation(minePin)
+        mapView.showAnnotations([minePin], animated: true)
+    }
+    
 }
 
